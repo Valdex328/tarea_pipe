77d7b489d534c3f6912bc05127ca8867fdcfb844
Daniel Valderrama Cuevas <valdex328@dayna-x.personal.edu.co>
2022-01-18 21:38:45 -0500

    Número aleatorio

--- pipe_act_3.c
+++ pipe_act_3.c
@@ -6,15 +6,17 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
 
 #define READ  0
 #define WRITE 1
 
 int main() {
-  srand((unsigned) time(NULL));
   pid_t pid;
   int   fd[2];
-  int numero_random = rand() %1001 + 1;
+  srand(time(NULL));
+  int numero_random;
+
   if (pipe(fd) == -1) {
 	perror("Creating pipe");
 	exit(EXIT_FAILURE);
@@ -24,27 +26,30 @@ int main() {
   if (pid == -1) {
 	perror("fork() failed)");
 	exit(EXIT_FAILURE);
-  } else if (pid == 1) {
-	// The parent process will execute.
-	// Close the pipe read descriptor.
-	close(fd[READ]);
-	// Redirect STDOUT to write to the pipe.
-	dup2(fd[WRITE], STDOUT_FILENO);
-	// Execute
-	numero_random = rand()%1001 +1;
+
+
   } else if (pid == 0) {
 	// The child process will execute wc.
 	// Close the pipe write descriptor.
 	close(fd[WRITE]);
-	// Redirect STDIN to read from the pipe.
+	// Redirect STDIN  read from the pipe.
 	dup2(fd[READ], STDIN_FILENO);
 	// Execute
-	printf("numero: %d \n", numero_random);
 	if (numero_random < 500){
 	   	 printf("menor que 500 \n");
 	} else if(numero_random >= 500) {
 	 	 printf("mayor o igual que 500 \n");
 	}
-     }
-}
 
+   } else if (pid > 0) {
+ 	// The parent process will execute.
+	// Close the pipe read descriptor.
+	close(fd[READ]);
+	// Redirect STDOUT to write to the pipe.
+	dup2(fd[WRITE], STDIN_FILENO);
+	// Execute
+	int numero_random = rand()%1000 + 1;
+	printf("numero: %d \n", numero_random);
+	return numero_random;
+  }
+}
